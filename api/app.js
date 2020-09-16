3537b856595e983ae36286134bbfd7480eb5bf9a
Patrick Carragher <carragher@live.se>
2020-09-16 19:26:37 +0200

    added cors exec

--- api/app.js
+++ api/app.js
@@ -1,10 +1,17 @@
 // Backend stuff
 
 const express = require('express')
-
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 
 const PORT = process.env.PORT || 1234;
+
+app.get('/', (req,res)=>{
+	res.json({
+		message:"IF THIS MESSAGE SHOWS THEN APP.GET WORKS ON THE BACKEND"
+	})
+})
 
 app.listen(PORT, () => console.log(`Server Online and now running on port:${PORT} `));
