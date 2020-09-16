6f57861a47dcd8245e8e0552d9cf199126a6a376
Patrick Carragher <carragher@live.se>
2020-09-16 19:13:43 +0200

    Cleaned up the frontend

---
+++ client/src/components/TestComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="test">
+    <h1>VUE IS THE BEST I LOVE IT!</h1>
+	<img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
