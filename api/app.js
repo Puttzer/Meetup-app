790d246ca7bc733b17ccf08de6dc176e9b650921
Patrick Carragher <carragher@live.se>
2020-09-22 17:59:45 +0200

    Making progress on backend

--- api/app.js
+++ api/app.js
@@ -1,13 +1,17 @@
 // Backend stuff
-
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const path = require("path");
+const eventDB = require("./routes/events")
 
-app.use(express.static(path.join(__dirname, "/public")));
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public", "index.html"))
-);
+  res.sendFile(path.join(__dirname, "/public", "index.html")));
+
+app.use("api/events", eventDB)  
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 1234;
 app.listen(PORT, () => console.log(`Server Online and now running on port ${PORT} `));
---
+++ api/routes/events.js
@@ -0,0 +1,19 @@
+const {Router} =require("express");
+const DataStore = require("nedb-promise")
+
+const router = new Router();
+const fs = require("fs");
+
+const EventDB = new DataStore({
+	filename: "db/events.db",
+	autoload: true
+});
+console.log(EventDB )
+
+router.get("/", async (req,res) =>{
+	const event = fs.createReadStream("./data/event.json")
+	event.pipe(res)
+	console.log(res)
+})
+
+module.exports=router;
