7d33511fb3bb1cb8e6bd22342bc88937a73e5062
Patrick Carragher <carragher@live.se>
2020-09-29 21:09:36 +0200

    basic header styling done

--- client/src/components/HeaderComponent.vue
+++ client/src/components/HeaderComponent.vue
@@ -1,10 +1,17 @@
 <template>
   <nav class="navbar">
     <img src="../assets/logo.png" alt="logo" class="logo" />
-      <ul class="nav-links">
-        <router-link class="home-route" to="/">Home</router-link>
-        <router-link class="events-route" to="/events">Events</router-link>
-      </ul>
+    <div class="links">
+      <router-link class="home-route" to="/" style="text-decoration: none"
+        >Home</router-link
+      >
+      <router-link
+        class="events-route"
+        to="/events"
+        style="text-decoration: none"
+        >Events</router-link
+      >
+    </div>
   </nav>
 </template>
 
@@ -15,5 +22,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Fira+Code&display=swap");
+.navbar {
+  display: flex;
+  align-items: center;
+  margin-right: auto;
+  color: #494892;
+  font-size: 20px;
+  font-family: "Fira Code", monospace;
+  background-color: blanchedalmond;
 
+  &:link {
+    color: #494892;
+  }
+}
+
+.links {
+  padding: 70px;
+  text-decoration: none;
+}
+
+.events-route {
+  padding: 110px;
+}
 </style>
