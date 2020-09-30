f4f97cddce4e6b850c56112662561d2f337712a3
Patrick Carragher <carragher@live.se>
2020-10-01 00:04:36 +0200

    FINAL FINAL yeah?

--- api/app.js
+++ api/app.js
@@ -9,12 +9,12 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) =>
-	res.sendFile(path.join(__dirname, "/public", "index.html")));
+// app.get("/", (req, res) =>
+// 	res.sendFile(path.join(__dirname, "/public", "index.html")));
 
-app.get("/events", (req, res) =>
-	res.sendFile(path.join(__dirname, "/public", "index.html")));
-app.use("/api/events", eventRoutes)
+// app.get("/events", (req, res) =>
+// 	res.sendFile(path.join(__dirname, "/public", "index.html")));
+// app.use("/api/events", eventRoutes)
 
 app.use(express.static(path.join(__dirname, '/dist')))
 app.get('*/', (req, res) => {
