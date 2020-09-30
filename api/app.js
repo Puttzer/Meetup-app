c665049e0643800217d894b9cc0e29e7108767f5
Patrick Carragher <carragher@live.se>
2020-09-30 21:09:14 +0200

    haha lol idgaf

--- api/app.js
+++ api/app.js
@@ -4,20 +4,21 @@ const cors = require('cors')
 const app = express()
 const path = require("path");
 const eventRoutes = require("./routes/events")
+const PORT = process.env.PORT || 1234;
 
+app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public", "index.html")));
-  
-app.get("/events", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public", "index.html")));
+// app.get("/", (req, res) =>
+//   res.sendFile(path.join(__dirname, "/public", "index.html")));
 
+// app.get("/events", (req, res) =>
+//   res.sendFile(path.join(__dirname, "/public", "index.html")));
 app.use("/api/events", eventRoutes)
-app.use(express.static(path.join(__dirname, "/public")));
-app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '/dist')))
+app.get('*/', (req, res) => {
+	res.sendFile(path.join(__dirname, '/dist', 'index.html'))
+})
 
-
-const PORT = process.env.PORT || 1234;
 app.listen(PORT, () => console.log(`Server Online and now running on port ${PORT} `));
